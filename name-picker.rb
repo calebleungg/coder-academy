3ab50d1917165e1fb96f25f195afc636889c1150
Caleb Leung <caleb.leungg@gmail.com>
2019-11-22 09:56:26 +1000

    optimising name picker

--- name-picker.rb
+++ name-picker.rb
@@ -1,31 +1,10 @@
 students = ["John", "Luke", "Picard", "Benjamin", "Sisko", "Kate", "Jane"] 
-picked = []
 
-
-def pick(students, picked)
-
-    no_of_students = students.length
-    pick = students[rand(0..(students.length - 1))]
-    until picked.include?(pick) == false
-        pick = students[rand(0..(students.length - 1))]
-    end
-
-    picked.push(pick)
-    return pick
+until students.length == 0
     
-end
-
-picking = true
-
-while picking
-
-    if picked.length == students.length
-        puts "Done!"
-        picking = false
-        return
-    end
-
-    puts "\n~~~~~ #{pick(students, picked)} ~~~~~"
+    puts "\n~~**<<|| #{students.shuffle!.slice!(0)} ||>>**~~"
     puts "\nPress Enter to pick again"
     gets
 end
+
+# random pick of names while deleting from 
